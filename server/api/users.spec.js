eb875ec87d712dee4faf3dc9befd2fdadabff1b0
Leslie Meng <mengli26@gmail.com>
2020-09-24 10:47:56 -0400

    added single user id spec

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -31,4 +31,42 @@ describe('User routes', () => {
       expect(res.body[0].email).to.be.equal(codysEmail)
     })
   }) // end describe('/api/users')
+  describe('/api/users/:id', () => {
+    const codysEmail = 'cody@puppybook.com'
+
+    beforeEach(() => {
+      User.create({
+        email: codysEmail,
+        firstName: 'Cody',
+        lastName: 'Dog'
+      })
+      User.create({
+        email: 'test@testing.com',
+        firstName: 'Robin',
+        lastName: 'Default'
+      })
+    })
+
+    it('GET /api/users/:id returns individual user data', async () => {
+      const res = await request(app)
+        .get('/api/users/1')
+        .expect(200)
+
+      expect(res.body).to.be.an('object')
+      expect(res.body.email).to.be.equal(codysEmail)
+    })
+    it('GET /api/users/:id will show different results for different ids', async () => {
+      const res = await request(app)
+        .get('/api/users/2')
+        .expect(200)
+
+      expect(res.body).to.be.an('object')
+      expect(res.body.firstName).to.be.equal('Robin')
+    })
+    it('returns Not Found if the id given is invalid', async () => {
+      const res = await request(app).get('/api/users/253')
+      expect(res.status).to.equal(404)
+      expect(res.text).to.be.equal('Not Found')
+    })
+  }) // end describe('/api/users/:id')
 }) // end describe('User routes')
