1b650efa940e79d7066788860617c96c42c8b864
Valerie Ingmann <valerieingmann707@gmail.com>
2020-09-25 18:50:02 -0400

    fixed bug with select dropdown

--- client/components/ActiveOrder.js
+++ client/components/ActiveOrder.js
@@ -38,24 +38,40 @@ class ActiveOrder extends React.Component {
       <div className="container">
         {!products.length
           ? 'No Products'
-          : products.map(product => {
+          : products.map((product, i) => {
               return (
                 <div key={product.id}>
-                  <h3>{product.name}</h3>
+                  <h3>
+                    {product.name}, {i}
+                  </h3>
                   <p>Description: {product.description}</p>
                   <p>Price: ${product.price / 100}</p>
                   <p>{product.OrderProducts.count}</p>
                   <select
                     onChange={e => this.handleChange(product, e)}
-                    defaultValue={product.OrderProducts.count}
+                    // defaultValue={i}
                   >
                     {selectArr.map(num => {
-                      return (
+                      return num === product.OrderProducts.count ? (
+                        <option selected key={num} value={num}>
+                          {num}
+                        </option>
+                      ) : (
                         <option key={num} value={num}>
                           {num}
                         </option>
                       )
                     })}
+                    {/* <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                    <option value={6}>6</option>
+                    <option value={7}>7</option>
+                    <option value={8}>8</option>
+                    <option value={9}>9</option>
+                    <option value={10}>10</option> */}
                   </select>
                   <p>Update Quantity</p>
                   <p>In Stock: {product.numOfItems}</p>
