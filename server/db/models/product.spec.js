3be576d4ba3c4c44c66df04da6f03f628c1d74b0
Valerie Ingmann <valerieingmann707@gmail.com>
2020-09-28 14:51:54 -0400

    renamed order file

--- server/db/models/product.spec.js
+++ server/db/models/product.spec.js
@@ -2,7 +2,7 @@ const {expect} = require('chai')
 const db = require('../index')
 const Product = db.model('product')
 
-describe('Product Model', () => {
+describe.only('Product Model', () => {
   let product
   before(() => db.sync({force: true}))
   beforeEach(() => {
