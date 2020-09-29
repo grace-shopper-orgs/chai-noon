8b23edbfc1ce4a14abe12776413188863a811553
Leslie Meng <mengli26@gmail.com>
2020-09-29 12:04:51 -0400

    added loading message for users page

--- client/components/AllUsers.js
+++ client/components/AllUsers.js
@@ -1,48 +1,74 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
+import {me} from '../store/user'
 import {fetchUsers} from '../store/users'
 import Footer from './Footer'
 
 export class AllUsers extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      loading: true
+    }
+  }
   componentDidMount() {
     this.props.getUsers()
+    this.props.getMe()
   }
-
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.user !== nextProps.user) {
+      this.setState({loading: false})
+      return true
+    }
+    if (this.state.loading !== nextState.loading) {
+      return true
+    }
+    return false
+  }
   render() {
-   
-    if (!this.props.user.isAdmin)
+    if (this.state.loading) {
+      return <div className="container">🍵 Fetching a kettle.... 🍵</div>
+    } else if (this.props.user.name === null)
+      return (
+        <div>
+          <Redirect to="/" />
+        </div>
+      )
+    else if (!this.props.user.isAdmin)
       return (
         <div className="section-title">
           Sorry, you do not have access to this page
         </div>
       )
+    else {
+      const {users} = this.props
 
-    const {users} = this.props
-
-    return (
-      <div>
-        <div className="section-title">
-          <br />
-          <br />
-          <h2>Users in the Database</h2>
-          <hr />
-          <br />
+      return (
+        <div>
+          <div className="section-title">
+            <br />
+            <br />
+            <h2>Users in the Database</h2>
+            <hr />
+            <br />
+          </div>
+          <div className="user-inline">
+            {!users.length
+              ? 'No Users'
+              : users.map(user => {
+                  return (
+                    <div className="user" key={user.id}>
+                      <p>User ID: {user.id}</p>
+                      <p>Email: {user.email}</p>
+                    </div>
+                  )
+                })}
+          </div>
+          <Footer />
         </div>
-        <div className="user-inline">
-          {!users.length
-            ? 'No Users'
-            : users.map(user => {
-                return (
-                  <div className="user" key={user.id}>
-                    <p>User ID: {user.id}</p>
-                    <p>Email: {user.email}</p>
-                  </div>
-                )
-              })}
-        </div>
-        <Footer />
-      </div>
-    )
+      )
+    }
   }
 }
 
@@ -57,6 +83,9 @@ const mapDispatch = dispatch => {
   return {
     getUsers: () => {
       dispatch(fetchUsers())
+    },
+    getMe: () => {
+      dispatch(me())
     }
   }
 }
